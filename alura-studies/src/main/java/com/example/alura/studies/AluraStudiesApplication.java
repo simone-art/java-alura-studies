87fe5a2c194914abcd266db34190a1486d175a76
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-06-14 16:38:24 -0300

    Configurando o projeto de estudos java alura

---
+++ alura-studies/src/main/java/com/example/alura/studies/AluraStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.alura.studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraStudiesApplication.class, args);
+	}
+
+}
---
+++ alura-studies/src/test/java/com/example/alura/studies/AluraStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alura.studies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
