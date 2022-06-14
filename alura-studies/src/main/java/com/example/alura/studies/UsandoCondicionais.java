d152579aa006f64e39548acce48babdf9a0a8ada
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-06-14 17:25:39 -0300

    Usando condicionais If e else no Java

---
+++ alura-studies/src/main/java/com/example/alura/studies/UsandoCondicionais.java
@@ -0,0 +1,16 @@
+package com.example.alura.studies;
+
+public class UsandoCondicionais {
+    public static void main(String[] args) {
+
+        System.out.println("testando condicionais");
+        int idade = 20;
+        if (idade >= 18) {
+            System.out.println("você tem mais de 18 anos");
+            System.out.println("seja bem vindo");
+        } else {
+            System.out.println("infelizmente você não pode entrar");
+        }
+    }
+}
+
