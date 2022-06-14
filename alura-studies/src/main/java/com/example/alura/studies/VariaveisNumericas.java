d90c814e8ee59bedfd88f50e41661bd5bb947687
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-06-14 16:58:04 -0300

    Declarando uma variável numérica int

---
+++ alura-studies/src/main/java/com/example/alura/studies/VariaveisNumericas.java
@@ -0,0 +1,17 @@
+package com.example.alura.studies;
+
+public class VariaveisNumericas {
+
+    public static void main(String[] args) {
+        int idade;
+        idade = 37;
+        System.out.println(idade);
+
+        idade = 30 + 10;
+        System.out.println(idade);
+
+        idade = (7 * 5) + 2;
+        System.out.println("A idade é: " + idade);
+
+    }
+}
