994077d3d84d6813534b84075131ac890ca80724
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-06-14 17:41:48 -0300

    Testando condicionais usando o operador lógico or || no Java

--- alura-studies/src/main/java/com/example/alura/studies/UsandoCondicionais.java
+++ alura-studies/src/main/java/com/example/alura/studies/UsandoCondicionais.java
@@ -5,12 +5,23 @@ public class UsandoCondicionais {
 
         System.out.println("testando condicionais");
         int idade = 20;
-        if (idade >= 18) {
+        int quantidadePessoas = 3;
+        /**
+         * O or || testa se uma das condições é verdadeira
+         * o and && testa se as duas são verdadeiras
+         */
+        if (idade >= 18 || quantidadePessoas >= 2) {
             System.out.println("você tem mais de 18 anos");
             System.out.println("seja bem vindo");
         } else {
+            if (quantidadePessoas <= 2)
             System.out.println("infelizmente você não pode entrar");
         }
+
+        int idadeIdoso = 68;
+        boolean ehIdoso = idadeIdoso >= 65;
+        System.out.println(ehIdoso);
+
     }
 }
 
