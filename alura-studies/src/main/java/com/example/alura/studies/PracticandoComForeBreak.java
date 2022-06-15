99e1f02491891ac62e7b550f466aa807770d978b
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-06-15 17:49:36 -0300

    O break apenas interrompe o laço de repetição mais interno

--- alura-studies/src/main/java/com/example/alura/studies/PracticandoComForeBreak.java
+++ alura-studies/src/main/java/com/example/alura/studies/PracticandoComForeBreak.java
@@ -5,6 +5,7 @@ public class PracticandoComForeBreak {
         for(int linha = 0; linha < 5; linha++) {
             for (int coluna = 0; coluna < 5; coluna++) {
                 if (coluna > linha) {
+                    // O break irá interromper apenas o laço de repetição mais interno que o contém.
                     break;
                 }
                 System.out.print( coluna+1);
