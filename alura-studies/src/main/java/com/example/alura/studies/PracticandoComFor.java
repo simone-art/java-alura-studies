c68509141c89d0dc68562b88e9e43555c944bc36
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-06-15 16:36:11 -0300

    Usando o for como estrutura de repetição

---
+++ alura-studies/src/main/java/com/example/alura/studies/PracticandoComFor.java
@@ -0,0 +1,13 @@
+package com.example.alura.studies;
+
+public class PracticandoComFor {
+    /**
+     * O for é um laço de repetição onde sua estrutura permite declarar a variável dentro dela
+     * Presta atenção que a variável não pode ser usada fora do for
+     */
+    public static void main(String[] args) {
+        for(int contador = 0; contador <= 10; contador++){
+            System.out.println(contador);
+        }
+    }
+}
