14dc63a99faa823fb1ff1091bc17fbb9d98ad005
s.santos.pimenta <simoneperiodista3@gmail.com>
2022-06-22 15:59:00 -0300

    Definindo os atributos de uma classe no Java

---
+++ alura-studies/src/main/java/com/example/alura/studies/ContaProgramacaoOrientadaAobjeto.java
@@ -0,0 +1,10 @@
+package com.example.alura.studies;
+
+public class ContaProgramacaoOrientadaAobjeto {
+    // Definindo os atributos da classe
+    // Class é uma palavra reservada do Java
+     double saldo;
+     int agencia;
+     int numero;
+     String titular;
+}
